cd8da29faf148e5b5a7fb5ebec2f4b21b83050fd
tainuth1 <tainuth1@gmail.com>
2025-06-05 13:41:25 +0700

    Filter Movie

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,17 @@ const Home = () => {
   const [searchParam] = useSearchParams();
   const page = searchParam.get("page") || 1;
   const [current, setCurrentPage] = useState(parseInt(page));
+  const [category, setCategory] = useState([]);
+
+  const [categoryValue, setCategoryValue] = useState("");
 
   const search = searchParam.get("search") || "";
 
   const getMovie = async () => {
     const query = search
       ? `https://api.themoviedb.org/3/search/movie?api_key=1d4fca46febab8295014ed7cfb3a54fd&query=${search}`
+      : categoryValue
+      ? `https://api.themoviedb.org/3/discover/movie?api_key=1d4fca46febab8295014ed7cfb3a54fd&with_genres=${categoryValue}`
       : `https://api.themoviedb.org/3/movie/popular?api_key=1d4fca46febab8295014ed7cfb3a54fd&page=${current}`;
 
     try {
@@ -31,20 +36,35 @@ const Home = () => {
     }
   };
 
+  const fetchCategory = async () => {
+    try {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=1d4fca46febab8295014ed7cfb3a54fd"
+      );
+
+      const data = await res.json();
+      setCategory(data.genres);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchCategory();
+
   useEffect(() => {
     getMovie();
-  }, [current, search]);
+  }, [current, search, categoryValue]);
 
   return (
     <div className="home-container">
       <div className="movies">
         <div className="header">
           <h2>Popular Movies</h2>
-          <select>
-            <option value="">All</option>
-            <option value="">Advancrture</option>
-            <option value="">Action</option>
-            <option value="">Cartoon</option>
+          <select onChange={(e) => setCategoryValue(e.target.value)}>
+            {category.map((cate) => (
+              <option key={cate.id} value={cate.id}>
+                {cate.name}
+              </option>
+            ))}
           </select>
         </div>
 
